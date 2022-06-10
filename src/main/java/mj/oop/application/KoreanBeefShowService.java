23857b1a5a0d875853e15b7173d3cca54fcaf55a
MJbae <bmj5208@gmail.com>
2022-06-10 23:14:22 +0900

    fix: KoreanBeefShowService에 service 어노테이션 추가하여 스프링 빈에 등록

--- src/main/java/mj/oop/application/KoreanBeefShowService.java
+++ src/main/java/mj/oop/application/KoreanBeefShowService.java
@@ -3,9 +3,11 @@ package mj.oop.application;
 import mj.oop.application.interfaces.ProductShowService;
 import mj.oop.domain.entity.KoreanBeef;
 import mj.oop.infra.KoreanBeefJpaRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class KoreanBeefShowService implements ProductShowService<KoreanBeef> {
     private final KoreanBeefJpaRepository repository;
 
