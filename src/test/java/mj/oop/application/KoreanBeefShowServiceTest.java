81937d635a75eb10addcbb8c4ca3e86a2288246e
MJbae <bmj5208@gmail.com>
2022-06-11 09:40:46 +0900

    test: KoreanBeefShowService::showBy에 대한 테스트 추가

--- src/test/java/mj/oop/application/KoreanBeefShowServiceTest.java
+++ src/test/java/mj/oop/application/KoreanBeefShowServiceTest.java
@@ -9,8 +9,11 @@ import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 
@@ -24,6 +27,7 @@ class KoreanBeefShowServiceTest {
     private final BigDecimal PRICE = new BigDecimal(1000);
     private final String MEAT_GRADE = "1+";
     private final Long PRODUCT_ID = 1L;
+    private final Long PRODUCT_ID_NOT_EXISTING = 100L;
 
     @BeforeEach
     void setUp() {
@@ -72,6 +76,49 @@ class KoreanBeefShowServiceTest {
             void it_returns_not_empty_list() {
                 assertThat(subject()).isNotEmpty();
             }
+        }
+    }
+
+    @Nested
+    @DisplayName("showBy 메소드는")
+    class Describe_showBy {
+        abstract class ContextShowingByExisting {
+            KoreanBeef withExisting() {
+                return service.showBy(PRODUCT_ID);
+            }
+        }
+
+        abstract class ContextShowingByNotExisting {
+            void withoutExisting() {
+                service.showBy(PRODUCT_ID_NOT_EXISTING);
+            }
+        }
+
+        @Nested
+        @DisplayName("만약 존재하는 상품의 ID를 전달 받는다면")
+        class Context_with_existing extends ContextShowingByExisting {
+            @BeforeEach
+            void setUp() {
+                given(repository.findById(PRODUCT_ID)).willReturn(Optional.of(product));
+            }
+
+            @Test
+            @DisplayName("전달 받은 Id에 해당하는 상품을 반환한다")
+            void it_returns_product_having_id_equal_to_param() {
+                assertThat(withExisting().getId()).isEqualTo(PRODUCT_ID);
+            }
+        }
+
+        @Nested
+        @DisplayName("만약 존재하지 않는 상품의 ID를 전달 받는다면")
+        class Context_with_not_existing extends ContextShowingByNotExisting {
+            @Test
+            @DisplayName("예외를 발생시킨다")
+            void it_throws_exception() {
+                assertThatThrownBy(this::withoutExisting)
+                        .isInstanceOf(NoSuchElementException.class);
+            }
+
         }
     }
 }
