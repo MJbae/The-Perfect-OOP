6c33cdd0dadd26b32e9e2b37ef00d9954e206053
MJbae <bmj5208@gmail.com>
2022-06-11 13:51:55 +0900

    feat: ProductCreateController 인터페이스에 따라 KoreanBeefCreateController 구현

---
+++ src/main/java/mj/oop/controller/KoreanBeefCreateController.java
@@ -0,0 +1,22 @@
+package mj.oop.controller;
+
+import mj.oop.controller.dto.KoreanBeefRequestData;
+import mj.oop.controller.dto.KoreanBeefResponseData;
+import mj.oop.controller.interfaces.ProductCreateController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+
+@RestController
+public class KoreanBeefCreateController implements ProductCreateController<KoreanBeefResponseData, KoreanBeefRequestData> {
+    @PostMapping("/beef")
+    @ResponseStatus(HttpStatus.CREATED)
+    @Override
+    public KoreanBeefResponseData create(@RequestBody KoreanBeefRequestData requestDto) {
+        return new KoreanBeefResponseData(1L, "맛있는 한우", new BigDecimal(1000), "1+");
+    }
+}
