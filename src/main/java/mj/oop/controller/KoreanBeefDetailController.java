c6674dc05dd9a2a6a41e0bea5ecb59c168eebe0a
MJbae <bmj5208@gmail.com>
2022-06-11 10:02:39 +0900

    feat: KoreanBeefDetailController의 stub 로직을 실제 구현으로 수정

--- src/main/java/mj/oop/controller/KoreanBeefDetailController.java
+++ src/main/java/mj/oop/controller/KoreanBeefDetailController.java
@@ -9,13 +9,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.math.BigDecimal;
 
 @RestController
 public class KoreanBeefDetailController implements ProductDetailController<KoreanBeefResponseData> {
+    private final ProductShowService<KoreanBeef> service;
+
+    public KoreanBeefDetailController(KoreanBeefShowService service) {
+        this.service = service;
+    }
+
     @GetMapping("/beef/{id}")
     @Override
     public KoreanBeefResponseData detail(@PathVariable Long id) {
-        return new KoreanBeefResponseData(1L, "맛있는 한우", new BigDecimal(1000), "1+");
+        KoreanBeef product = service.showBy(id);
+        return KoreanBeefResponseData.from(product);
     }
 }
