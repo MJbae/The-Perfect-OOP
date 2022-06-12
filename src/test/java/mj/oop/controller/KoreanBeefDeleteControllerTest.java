41f3764a68ec855ebe610dc5481beb3b150b5c56
MJbae <bmj5208@gmail.com>
2022-06-12 11:26:15 +0900

    test: 한우상품 삭제 요청 테스트에 존재하지 않은 한우상품을 매개변수로 전달할 경우 예외발생여부 테스트 추가

--- src/test/java/mj/oop/controller/KoreanBeefDeleteControllerTest.java
+++ src/test/java/mj/oop/controller/KoreanBeefDeleteControllerTest.java
@@ -1,15 +1,21 @@
 package mj.oop.controller;
 
 
+import mj.oop.application.KoreanBeefDeleteService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 
+import java.util.NoSuchElementException;
+
+import static org.mockito.BDDMockito.willThrow;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -18,10 +24,13 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @WebMvcTest(KoreanBeefDeleteController.class)
 @DisplayName("KoreanBeefDeleteController")
 class KoreanBeefDeleteControllerTest {
+    @MockBean
+    private KoreanBeefDeleteService service;
     @Autowired
     private MockMvc mockMvc;
 
     private final Long PRODUCT_ID = 1L;
+    private final Long PRODUCT_ID_NOT_EXISTING = 100L;
 
     @Nested
     @DisplayName("delete 메소드는")
@@ -36,5 +45,25 @@ class KoreanBeefDeleteControllerTest {
                         .andExpect(status().isNoContent());
             }
         }
+
+        @Nested
+        @DisplayName("만약 존재하지 않는 ID를 매개변수로 전달 받는다면")
+        class Context_without_existing_product {
+            @BeforeEach
+            void setUp() {
+                willThrow(new NoSuchElementException(PRODUCT_ID_NOT_EXISTING.toString()))
+                        .given(service).deleteBy(PRODUCT_ID_NOT_EXISTING);
+            }
+
+            @Test
+            @DisplayName("HTTP Status Code 404 NOT FOUND 응답한다")
+            void it_responds_with_404() throws Exception {
+                mockMvc.perform(delete("/beef/" + PRODUCT_ID_NOT_EXISTING))
+                        .andExpect(status().isNotFound());
+            }
+
+        }
     }
+
+
 }
