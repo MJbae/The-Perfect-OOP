9aa377004a762d1463cc1b458cef9bedc49ce9f8
MJbae <bmj5208@gmail.com>
2022-06-12 11:20:28 +0900

    test: KoreanBeefDeleteService에 존재하지 않은 한우 상품에 대한 삭제 요청 시 예외발생여부 테스트 추가

--- src/test/java/mj/oop/application/KoreanBeefDeleteServiceTest.java
+++ src/test/java/mj/oop/application/KoreanBeefDeleteServiceTest.java
@@ -1,15 +1,15 @@
 package mj.oop.application;
 
 import mj.oop.application.interfaces.ProductDeleteService;
-import mj.oop.domain.entity.KoreanBeef;
 import mj.oop.infra.KoreanBeefJpaRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-import java.math.BigDecimal;
+import java.util.NoSuchElementException;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 
@@ -18,11 +18,6 @@ import static org.mockito.Mockito.mock;
 class KoreanBeefDeleteServiceTest {
     private final KoreanBeefJpaRepository repository = mock(KoreanBeefJpaRepository.class);
     private ProductDeleteService service;
-    private KoreanBeef product;
-
-    private final String PRODUCT_NAME = "세상에서 제일 맛있는 한우";
-    private final BigDecimal PRICE = new BigDecimal(1000);
-    private final String MEAT_GRADE = "1+";
     private final Long PRODUCT_ID = 1L;
     private final Long PRODUCT_ID_NOT_EXISTING = 100L;
 
@@ -58,6 +53,22 @@ class KoreanBeefDeleteServiceTest {
             @DisplayName("값을 반환하지 않는다")
             void it_returns_nothing() {
                 withExisting();
+            }
+        }
+
+        @Nested
+        @DisplayName("만약 존재하지 않는 상품을 삭제한다면")
+        class Context_with_not_existing_product extends ContextDeletingNotExisting {
+            @BeforeEach
+            void setUp() {
+                given(repository.existsById(PRODUCT_ID_NOT_EXISTING)).willReturn(Boolean.FALSE);
+            }
+
+            @Test
+            @DisplayName("예외를 발생시킨다")
+            void it_throws_exception() {
+                assertThatThrownBy(this::withoutExisting)
+                        .isInstanceOf(NoSuchElementException.class);
             }
         }
     }
