140b94cc11dabcba3571ef2c332ce126b05e6607
MJbae <bmj5208@gmail.com>
2022-06-12 17:58:45 +0900

    feat: 사용자 저장소 정의

---
+++ src/main/java/mj/oop/infra/UserJpaRepository.java
@@ -0,0 +1,59 @@
+package mj.oop.infra;
+
+import mj.oop.domain.entity.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * '사용자' 저장소 인터페이스
+ */
+@Repository
+public interface UserJpaRepository extends CrudRepository<User, Long> {
+    /**
+     * 모든 사용자를 반환한다
+     * <p>
+     *
+     * @return 사용자를 내부 요소로 하는 List
+     * </p>
+     */
+    List<User> findAll();
+
+    /**
+     * id에 해당하는 사용자를 반환한다
+     * <p>
+     * @param id 사용자 id
+     * @return Optional<User> 사용자
+     * </p>
+     */
+    Optional<User> findById(Long id);
+
+    /**
+     * 사용자를 저장하고, 저장된 사용자를 반환한다
+     * <p>
+     * @param user 사용자
+     * @return 사용자
+     * </p>
+     */
+    User save(User user);
+
+
+    /**
+     * id에 해당하는 사용자를 삭제한다
+     * <p>
+     * @param id 사용자 id
+     * </p>
+     */
+    void deleteById(Long id);
+
+    /**
+     * id에 해당하는 사용자 존재여부를 반환한다
+     * <p>
+     * @param id 사용자 id
+     * @return 사용자 존재여부
+     * </p>
+     */
+    boolean existsById(Long id);
+}
