1a604707f0eebe1f860f9efd2007ae85f5c19575
MJbae <bmj5208@gmail.com>
2022-06-12 11:26:41 +0900

    feat: KoreanBeefDeleteController에 Stub 로직을 실구현으로 변경

--- src/main/java/mj/oop/controller/KoreanBeefDeleteController.java
+++ src/main/java/mj/oop/controller/KoreanBeefDeleteController.java
@@ -1,5 +1,7 @@
 package mj.oop.controller;
 
+import mj.oop.application.KoreanBeefDeleteService;
+import mj.oop.application.interfaces.ProductDeleteService;
 import mj.oop.controller.interfaces.ProductDeleteController;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -7,15 +9,17 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class KoreanBeefDeleteController implements ProductDeleteController {
-//    private final ProductDeleteService service;
-//    public KoreanBeefDeleteController(KoreanBeefDeleteService service) {
-//        this.service = service;
-//    }
+    private final ProductDeleteService service;
+
+    public KoreanBeefDeleteController(KoreanBeefDeleteService service) {
+        this.service = service;
+    }
 
     @DeleteMapping("/beef/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @Override
     public void delete(@PathVariable Long id) {
+        service.deleteBy(id);
     }
 
 }
