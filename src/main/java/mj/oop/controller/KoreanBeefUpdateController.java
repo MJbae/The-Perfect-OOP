f3a0cc1b45fbe113f4aceb97a6eb0f04c7a811eb
MJbae <bmj5208@gmail.com>
2022-06-12 10:52:17 +0900

    feat: KoreanBeefUpdateController 내 stub 로직을 실구현으로 대체

--- src/main/java/mj/oop/controller/KoreanBeefUpdateController.java
+++ src/main/java/mj/oop/controller/KoreanBeefUpdateController.java
@@ -1,19 +1,27 @@
 package mj.oop.controller;
 
+import mj.oop.application.KoreanBeefUpdateService;
+import mj.oop.application.interfaces.ProductUpdateService;
 import mj.oop.controller.dto.KoreanBeefRequestData;
 import mj.oop.controller.dto.KoreanBeefResponseData;
 import mj.oop.controller.interfaces.ProductUpdateController;
+import mj.oop.domain.entity.KoreanBeef;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.math.BigDecimal;
 
 @RestController
 public class KoreanBeefUpdateController implements ProductUpdateController<KoreanBeefResponseData, KoreanBeefRequestData> {
+    private final ProductUpdateService<KoreanBeef> service;
+
+    public KoreanBeefUpdateController(KoreanBeefUpdateService service) {
+        this.service = service;
+    }
 
     @PatchMapping("/beef/{id}")
     @Override
     public KoreanBeefResponseData update(@PathVariable Long id, @RequestBody @Valid KoreanBeefRequestData requestData) {
-        return new KoreanBeefResponseData(id, "맛있는 한우", new BigDecimal(1000), "1+");
+        KoreanBeef product = service.update(id, requestData.toEntity());
+        return KoreanBeefResponseData.from(product);
     }
 }
