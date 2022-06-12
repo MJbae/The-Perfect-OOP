7f8cc0d849f0ba4c8b0d0074ec7bfd2e277d72e2
MJbae <bmj5208@gmail.com>
2022-06-12 11:06:19 +0900

    feat: KoreanBeefDeleteController stub 구현

---
+++ src/main/java/mj/oop/controller/KoreanBeefDeleteController.java
@@ -0,0 +1,21 @@
+package mj.oop.controller;
+
+import mj.oop.controller.interfaces.ProductDeleteController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+public class KoreanBeefDeleteController implements ProductDeleteController {
+//    private final ProductDeleteService service;
+//    public KoreanBeefDeleteController(KoreanBeefDeleteService service) {
+//        this.service = service;
+//    }
+
+    @DeleteMapping("/beef/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @Override
+    public void delete(@PathVariable Long id) {
+    }
+
+}
