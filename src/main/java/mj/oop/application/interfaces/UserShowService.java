2b3e428ffc61a4845cf4a1a357727ad4b96642fe
MJbae <bmj5208@gmail.com>
2022-06-12 18:00:18 +0900

    feat: 사용자 객체 대상 조회의 역할에 대한 인터페이스 정의

---
+++ src/main/java/mj/oop/application/interfaces/UserShowService.java
@@ -0,0 +1,31 @@
+package mj.oop.application.interfaces;
+
+import mj.oop.domain.entity.User;
+
+import java.util.List;
+
+/**
+ * User 타입 조회에 대한 비지니스 로직을 처리한다
+ * <p>
+ * All Known Implementing Classes:
+ * UserCrudService
+ * </p>
+ */
+public interface UserShowService {
+    /**
+     * 모든 User 엔티티를 List 형태로 반환한다
+     * <p>
+     * @return User 엔티티를 내부 요소로 하는 List Collection
+     * </p>
+     */
+    List<User> showAll();
+
+    /**
+     * 매개변수로 전달 받은 id에 해당하는 User 엔티티를 반환한다
+     * <p>
+     * @param id User 엔티티의 Id에 해당
+     * @return User 엔티티
+     * </p>
+     */
+    User showById(Long id);
+}
