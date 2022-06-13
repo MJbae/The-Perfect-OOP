f6cb1e6d765b737e636d4a0c5e905115f2d0bedd
MJbae <bmj5208@gmail.com>
2022-06-13 14:44:48 +0900

    fix: User는 DB에서 예약어이므로 User 클래스의 이름을 DB 상 users로 표기되도록 수정

--- src/main/java/mj/oop/domain/entity/User.java
+++ src/main/java/mj/oop/domain/entity/User.java
@@ -8,10 +8,12 @@ import lombok.NoArgsConstructor;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
 @Getter
 @Builder
+@Table(name = "users")
 @NoArgsConstructor
 @AllArgsConstructor
 public class User {
